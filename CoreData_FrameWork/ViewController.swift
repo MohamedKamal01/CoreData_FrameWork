a4b64f3493b2f48777c35fd397c12fb5f2658e66
MohamedKamal01 <mohamedkamal01099575034@gmail.com>
2022-04-15 14:08:36 +0200

    Initial Commit

---
+++ CoreData_FrameWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_FrameWork
+//
+//  Created by Mohamed Kamal on 15/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
